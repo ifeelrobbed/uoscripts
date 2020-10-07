5d7b18648bd4ea32dcdc8b7b220aeb9d8629e31b
ifeelrobbed <cameronrj@gmail.com>
2020-10-06 19:47:24 -0500

    cleanup and create shortcut

--- uogo/uogo.ps1
+++ uogo/uogo.ps1
@@ -1,3 +1,4 @@
+$ocuPath="C:\Program Files (x86)\Electronic Arts\Ultima Online Classic\UO.exe"
 $occPath="C:\Program Files (x86)\Electronic Arts\Ultima Online Classic\client.exe"
 $occInstaller="http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe"
 $occExe="UOClassicSetup_7_0_24_0.exe"
@@ -11,14 +12,42 @@ $cuoJsonUrl="https://raw.githubusercontent.com/ifeelrobbed/uoscripts/main/uogo/s
 $caUrl="https://github.com/Reetus/ClassicAssist/releases/download/0.3.90.233/ClassicAssist.zip"
 $caFile="ClassicAssist.zip"
 
-New-Item -ItemType directory -Path $dlLoc
+$mapUrl="https://github.com/ifeelrobbed/uoscripts/raw/main/uogo/maps/map.zip"
+$mapFile="map.zip"
 
+Write-Host "*****************************************************************************************" -ForegroundColor yellow
+Write-Host "This script will do the following:" -ForegroundColor yellow
+Write-Host "Check if you have the official UO Classic Client installed and updated." -ForegroundColor yellow
+Write-Host "If not, it will prompt you on how to download and install." -ForegroundColor yellow
+Write-Host "If so, it will then:" -ForegroundColor yellow
+Write-Host "Download and install the ClassicUO client to bring UO closer to 2020 rather than 1999." -ForegroundColor yellow
+Write-Host "Configure ClassicUO client to connect to the ProjectArcturus shard." -ForegroundColor yellow
+Write-Host "Download and install ClassicAssist to allow powerful macro creation and misc options." -ForegroundColor yellow
+Write-Host "Download and install world map data so towns, shops, dungeons, etc are marked on the map." -ForegroundColor yellow
+Write-Host "*****************************************************************************************" -ForegroundColor yellow
+<#
+if (-not (Test-Path $dlLoc -PathType Container)) {
+	Write-Host "Creating download directory $dlLoc"
+	New-Item -ItemType directory -Path $dlLoc | Out-Null
+	}
+	else {
+		Write-Host "$dlLoc already exists"
+	}
 if (Test-Path $occPath -PathType Leaf) {
 	Write-Host "`nClassic Client found at $occPath"}
-else {
-	Write-Host "`nClassic Client not found!" -ForegroundColor red
-	Write-Host "`n`nPlease download and install `n$occInstaller `n`nThen run this script again.`n`n"
-	exit}
+	elseif ((Test-Path $ocuPath -PathType Leaf) -and (-not(Test-Path $occPath -PathType Leaf))) {
+		Write-Host "`nClassic Client found but not updated!" -ForegroundColor red
+		Write-Host "`n`nPlease run the offical UO client:`n$ocuPath `nLet it finish updating. Then run this script again.`n`n" -ForegroundColor red
+		
+		Start-Sleep -s 10
+		
+		ii "C:\Program Files (x86)\Electronic Arts\Ultima Online Classic"		
+		
+		exit}
+	else {
+		Write-Host "`nClassic Client not found!" -ForegroundColor red
+		Write-Host "`n`nPlease download and install `n$occInstaller `n`nOnce installed, run: `n$ocuPath `n`nand let it finish updating. Once updated, close the update window and run this script again.`n`n" -ForegroundColor red
+		exit}
 
 $cuoLoc="$dlLoc\$cuoFile"
 Write-Host "Downloading ClassicUO client to $cuoLoc"	
@@ -37,3 +66,25 @@ Expand-Archive -Path $caLoc -DestinationPath "$dlLoc\ClassicAssist\" -Force
 $cuoJsonLoc="$dlLoc\ClassicUO\settings.json"
 Write-Host "Downloading settings file"
 Invoke-WebRequest -Uri $cuoJsonUrl -OutFile $cuoJsonLoc
+
+$mapLoc="$dlLoc\$mapFile"
+Write-Host "Downloading map files"
+Invoke-WebRequest -Uri $mapUrl -OutFile $mapLoc
+
+Write-Host "Extracting Map data to $dlLoc\ClassicUO\Data\Client\"
+Expand-Archive -Path $mapLoc -DestinationPath "$dlLoc\ClassicUO\Data\Client\" -Force
+#>
+$cuoExe="$dlLoc\ClassicUO\ClassicUO.exe"
+
+Write-Host "Creating Desktop shortcut for ClassicUO"
+$desktop=[environment]::getfolderpath("Desktop")
+$shortCut="$desktop\ClassicUO.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($shortCut)
+$Shortcut.TargetPath = $cuoExe
+$Shortcut.Save()
+
+Write-Host "**********************************************************************************************************************" -ForegroundColor green
+Write-Host "Install complete!`nYou may now launch the game and connect to the ProjectArcturus shard via the ClassicUO Desktop shortcut or by running:`n$cuoExe" -ForegroundColor green
+Write-Host "**********************************************************************************************************************" -ForegroundColor green
+
