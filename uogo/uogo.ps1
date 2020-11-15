7cba2fc8ef3997b4fcd2cd2b3e1e8e2ea4caf9a8
ifeelrobbed <67766369+ifeelrobbed@users.noreply.github.com>
2020-11-15 13:22:50 -0600

    Updated to latest CUO and CA

--- uogo/uogo.ps1
+++ uogo/uogo.ps1
@@ -5,11 +5,11 @@ $occExe="UOClassicSetup_7_0_24_0.exe"
 $dlLoc="C:\UO"
 #$dlLoc="C:\Temp"
 
-$cuoUrl="https://github.com/andreakarasho/ClassicUO/releases/download/0.1.5.000/ClassicUO-beta-0-1-5-000-release.zip"
-$cuoFile="ClassicUO-beta-0-1-5-000-release.zip"
+$cuoUrl="https://github.com/andreakarasho/ClassicUO/releases/download/0.1.6.1/ClassicUO-beta-0-1-6-1-release.zip"
+$cuoFile="ClassicUO-beta-0-1-6-1-release.zip"
 $cuoJsonUrl="https://raw.githubusercontent.com/ifeelrobbed/uoscripts/main/uogo/settings.json"
 
-$caUrl="https://github.com/Reetus/ClassicAssist/releases/download/0.3.90.233/ClassicAssist.zip"
+$caUrl="https://github.com/Reetus/ClassicAssist/releases/download/0.3.129.240/ClassicAssist.zip"
 $caFile="ClassicAssist.zip"
 
 $mapUrl="https://github.com/ifeelrobbed/uoscripts/raw/main/uogo/maps/map.zip"
@@ -87,7 +87,4 @@ $Shortcut.Save()
 Write-Host "**********************************************************************************************************************" -ForegroundColor green
 Write-Host "Install complete!`nYou may now launch the game and connect to the ProjectArcturus shard via the ClassicUO Desktop shortcut or by running:`n$cuoExe" -ForegroundColor green
 Write-Host "**********************************************************************************************************************" -ForegroundColor green
-
-Read-Host -Prompt "Press Enter to exit"
-
 
